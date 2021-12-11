da4dff823144a2fbf9a8f01c638b3a754ef08ac5
Scott Field <safield14@gmail.com>
2021-12-11 16:03:38 -0600

    Print refreshToken to console.

--- bin/questrade_tool.dart
+++ bin/questrade_tool.dart
@@ -1,8 +1,18 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
 import 'questrade.dart';
 
 void main(List<String> arguments) async {
 
-  var accessToken = await Questrade.getAccessToken("");
+  if (arguments.length != 1) {
+    print("ERROR: expecting a single argument that contains the refresh token");
+  }
+  var accessToken = await Questrade.getAccessToken(arguments[0]);
+  var newRefreshToken = accessToken.refreshToken;
+
+  print("New Refresh Token:");
+  print(newRefreshToken);
+  print("complete!");
 }
 
 
