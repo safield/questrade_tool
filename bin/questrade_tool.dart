8922175d22fd17e2eaff2538f1d80418bf5f35f5
Scott Field <safield14@gmail.com>
2021-12-19 19:51:08 -0600

    Summarize positions.

--- bin/questrade_tool.dart
+++ bin/questrade_tool.dart
@@ -1,5 +1,11 @@
 import 'questrade.dart';
 
+class AccountPosition {
+  final Account account;
+  final Position position;
+  AccountPosition(this.account , this.position);
+}
+
 void main(List<String> arguments) async {
 
   if (arguments.length != 1) {
@@ -15,15 +21,38 @@ void main(List<String> arguments) async {
 
   var accounts =  await Questrade.getAccounts(accessToken);
   print(accounts.toString());
+  double portfolioValue = 0.0;
 
-  List<Position> positions = [];
+  Map<String , List<AccountPosition>> positionsSummary = {};
   for (var account in accounts) {
-    var accountsPositions =  await Questrade.getPositions(accessToken , account.number);
-    positions.addAll(accountsPositions);
+    var positions =  await Questrade.getPositions(accessToken , account.number);
+    for (var position in positions) {
+      portfolioValue += position.currentPrice * position.openQuantity;
+      var positionSummary = positionsSummary.putIfAbsent(position.symbol, () => []);
+      positionSummary.add(AccountPosition(account , position));
+    }
   }
 
-  for (var position in positions) {
-    print(position);
+  for (var accountPositionsList in positionsSummary.values) {
+    String symbol = accountPositionsList.first.position.symbol;
+    num currentPrice = accountPositionsList.first.position.currentPrice;
+    int openQuantity = 0;
+    num closedPnl = 0;
+    num openPnl = 0;
+    int accounts = 0;
+    for (var accountPosition in accountPositionsList) {
+      var position = accountPosition.position;
+      accounts++;
+      assert(position.symbol == symbol);
+      assert(position.currentPrice == currentPrice);
+      openQuantity += position.openQuantity;
+      closedPnl += position.closedPnl;
+      openPnl += position.openPnl;
+    }
+
+    num totalValue = openQuantity * currentPrice;
+    double portfolioPercentage = totalValue / portfolioValue * 100;
+    print("$symbol currentPrice=$currentPrice quantity=$openQuantity openPnl=$openPnl closedPnl=$closedPnl totalValue=$totalValue portfolioPecentage=${portfolioPercentage.toStringAsFixed(2)} accounts=$accounts");
   }
 }
 
