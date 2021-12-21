5b37f0c1e9d0325b57d177ff340a90928393ebd6
Scott Field <safield14@gmail.com>
2021-12-20 20:18:25 -0600

    Added sort by portfolioPercentage.

--- bin/questrade_tool.dart
+++ bin/questrade_tool.dart
@@ -3,11 +3,31 @@ import 'questrade.dart';
 class AccountPosition {
   final Account account;
   final Position position;
-  AccountPosition(this.account , this.position);
+  AccountPosition(this.account, this.position);
+}
+
+class PositionSummary {
+  final String symbol;
+  final num currentPrice;
+  final int openQuantity;
+  final num openPnl;
+  final int accounts;
+  final double totalPortfolioValue;
+
+  num get totalValue => openQuantity * currentPrice;
+  num get portfolioPercentage => totalValue / totalPortfolioValue * 100;
+
+  PositionSummary({
+    required this.symbol,
+    required this.currentPrice,
+    required this.openQuantity,
+    required this.openPnl,
+    required this.accounts,
+    required this.totalPortfolioValue,
+  });
 }
 
 void main(List<String> arguments) async {
-
   if (arguments.length != 1) {
     print("ERROR: expecting a single argument that contains the refresh token");
   }
@@ -19,19 +39,21 @@ void main(List<String> arguments) async {
   print(newRefreshToken);
   print("complete!");
 
-  var accounts =  await Questrade.getAccounts(accessToken);
+  var accounts = await Questrade.getAccounts(accessToken);
   print(accounts.toString());
   double portfolioValue = 0.0;
 
-  Map<String , List<AccountPosition>> positionsSummary = {};
+  Map<String, List<AccountPosition>> positionsSummary = {};
   for (var account in accounts) {
-    var positions =  await Questrade.getPositions(accessToken , account.number);
+    var positions = await Questrade.getPositions(accessToken, account.number);
     for (var position in positions) {
       portfolioValue += position.currentPrice * position.openQuantity;
       var positionSummary = positionsSummary.putIfAbsent(position.symbol, () => []);
-      positionSummary.add(AccountPosition(account , position));
+      positionSummary.add(AccountPosition(account, position));
     }
   }
+
+  List<PositionSummary> summaries = [];
 
   for (var accountPositionsList in positionsSummary.values) {
     String symbol = accountPositionsList.first.position.symbol;
@@ -50,10 +72,19 @@ void main(List<String> arguments) async {
       openPnl += position.openPnl;
     }
 
-    num totalValue = openQuantity * currentPrice;
-    double portfolioPercentage = totalValue / portfolioValue * 100;
-    print("$symbol currentPrice=$currentPrice quantity=$openQuantity openPnl=$openPnl closedPnl=$closedPnl totalValue=$totalValue portfolioPecentage=${portfolioPercentage.toStringAsFixed(2)} accounts=$accounts");
+    summaries.add(PositionSummary(
+      symbol: symbol,
+      currentPrice: currentPrice,
+      openQuantity: openQuantity,
+      openPnl: openPnl,
+      accounts: accounts,
+      totalPortfolioValue: portfolioValue,
+    ));
+  }
+
+  summaries.sort((a , b) =>  b.portfolioPercentage.compareTo(a.portfolioPercentage));
+
+  for (var summary in summaries) {
+    print("${summary.symbol} currentPrice=${summary.currentPrice} quantity=${summary.openQuantity} openPnl=${summary.openPnl} totalValue=${summary.totalValue} portfolioPecentage=${summary.portfolioPercentage.toStringAsFixed(2)} accounts=${summary.accounts}");
   }
 }
-
-
